f5104a9c840ffb230512366aee3b3552fb8014de
CaVaAnWl <antonin@wlodarczyk.fr>
2024-09-23 19:05:19 +0200

    Update search.js

--- scripts/search.js
+++ scripts/search.js
@@ -5,6 +5,11 @@ function go() {
     if (recup==="Chute de fruits") {
         window.location.href="games/chuteDeFruits/chute de fruits.html";
     }
+    
+    else if (recup==="pacman") {
+        window.location.href="https://www.google.fr/logos/2010/pacman10-hp.html#bg=black";
+    }
+    
     else {
         window.location.href="games/"+recup+".html";
     }
